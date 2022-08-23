0c70a91f7b4224bf995a9452bcf769230723a3be
asiamohammed <mohamedasia021@gmail.com>
2022-08-23 17:47:03 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+const a = ["Guadalupe", "Ollie", "Aki"] ;
+const newArray =[];
+
+function writeCards(a, event){
+    
+
+    for (let i=0; i< a.length ; i++){
+        newArray.push (`Thank you, ${a[i]}, for the wonderful ${event} gift!`)
+    }
+    return newArray
+}
+ function countDown(num){
+    let countup = 0;
+    while ( countup <= num){
+        console.log(countup++)
+    }
+ }
